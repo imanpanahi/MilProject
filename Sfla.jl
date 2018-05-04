b91857875de7cd494ee874d7778396879fe6b9ec
Unknown <iman.panahi.c@gmail.com>
2018-05-04 02:11:52 -0700

    added sfla script file

---
+++ Sfla.jl
@@ -0,0 +1,13 @@
+###### initialize paramaters
+m= # number of memplex
+n= # number of frog in each memplex
+
+###### generate initial population
+
+###### partition population into memplexs
+
+###### memetic evolution withing each memplex
+
+##### shuffling all frogs
+
+#### check termination consdition
